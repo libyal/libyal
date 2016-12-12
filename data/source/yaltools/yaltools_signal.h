445951f4181f0f6daffb84837f56f6eba027dd2b
Joachim Metz <joachim.metz@gmail.com>
2016-12-12 21:48:33 +0100

    Worked on source generate script

---
+++ data/source/yaltools/yaltools_signal.h
@@ -0,0 +1,72 @@
+/*
+ * Signal handling functions
+ *
+ * Copyright (C) ${copyright}, ${tools_authors}
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined( _${tools_name_upper_case}_SIGNAL_H )
+#define _${tools_name_upper_case}_SIGNAL_H
+
+#include <common.h>
+#include <types.h>
+
+#include "${tools_name}_libcerror.h"
+
+#if defined( __cplusplus )
+extern "C" {
+#endif
+
+#if !defined( HAVE_SIGNAL_H ) && !defined( WINAPI )
+#error missing signal functions
+#endif
+
+#if defined( WINAPI )
+typedef unsigned long ${tools_name}_signal_t;
+
+#else
+typedef int ${tools_name}_signal_t;
+
+#endif /* defined( WINAPI ) */
+
+#if defined( WINAPI )
+
+BOOL WINAPI ${tools_name}_signal_handler(
+             ${tools_name}_signal_t signal );
+
+#if defined( _MSC_VER )
+
+void ${tools_name}_signal_initialize_memory_debug(
+      void );
+
+#endif /* defined( _MSC_VER ) */
+
+#endif /* defined( WINAPI ) */
+
+int ${tools_name}_signal_attach(
+     void (*signal_handler)( ${tools_name}_signal_t ),
+     libcerror_error_t **error );
+
+int ${tools_name}_signal_detach(
+     libcerror_error_t **error );
+
+#if defined( __cplusplus )
+}
+#endif
+
+#endif /* !defined( _${tools_name_upper_case}_SIGNAL_H ) */
+
---
+++ data/source/yaltools/yaltools_unused.h
@@ -0,0 +1,50 @@
+/*
+ * The unused definition
+ *
+ * Copyright (C) ${copyright}, ${tools_authors}
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined( _${tools_name_upper_case}_TEST_UNUSED_H )
+#define _${tools_name_upper_case}_TEST_UNUSED_H
+
+#include <common.h>
+
+#if !defined( ${tools_name_upper_case}_TEST_ATTRIBUTE_UNUSED )
+
+#if defined( __GNUC__ ) && __GNUC__ >= 3
+#define ${tools_name_upper_case}_TEST_ATTRIBUTE_UNUSED	__attribute__ ((__unused__))
+
+#else
+#define ${tools_name_upper_case}_TEST_ATTRIBUTE_UNUSED
+
+#endif /* defined( __GNUC__ ) && __GNUC__ >= 3 */
+
+#endif /* !defined( ${tools_name_upper_case}_TEST_ATTRIBUTE_UNUSED ) */
+
+#if defined( _MSC_VER )
+#define ${tools_name_upper_case}_TEST_UNREFERENCED_PARAMETER( parameter ) \
+	UNREFERENCED_PARAMETER( parameter );
+
+#else
+#define ${tools_name_upper_case}_TEST_UNREFERENCED_PARAMETER( parameter ) \
+	/* parameter */
+
+#endif /* defined( _MSC_VER ) */
+
+#endif /* !defined( _${tools_name_upper_case}_TEST_UNUSED_H ) */
+
