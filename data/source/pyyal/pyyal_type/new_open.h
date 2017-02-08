7865306167eac1d8300b60f82be430995e5a6dbe
Joachim Metz <joachim.metz@gmail.com>
2017-02-08 08:12:28 +0100

    Worked on source generate script

--- data/source/pyyal/pyyal_type/new_open.h
+++ data/source/pyyal/pyyal_type/new_open.h
@@ -3,7 +3,7 @@ PyObject *${python_module_name}_${type_name}_new_open(
            PyObject *arguments,
            PyObject *keywords );
 
-PyObject *${python_module_name}_${type_name}_new_open_${type_name}_object(
+PyObject *${python_module_name}_${type_name}_new_open_file_object(
            PyObject *self,
            PyObject *arguments,
            PyObject *keywords );
