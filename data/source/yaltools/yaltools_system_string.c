3c8009ad94d5799708ca76356154b9d8dcfaa950
Joachim Metz <joachim.metz@gmail.com>
2017-03-05 09:07:43 +0100

    Worked on source generate script

---
+++ data/source/yaltools/yaltools_system_string.c
@@ -0,0 +1,138 @@
+/*
+ * Character string functions
+ *
+ * Copyright (C) ${copyright}, ${tools_authors}
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+#include <memory.h>
+#include <types.h>
+
+#include "${tools_name}_libcerror.h"
+#include "${tools_name}_system_string.h"
+
+/* Copies a string of a decimal value to a 64-bit value
+ * Returns 1 if successful or -1 on error
+ */
+int ${tools_name}_system_string_decimal_copy_to_64_bit(
+     const system_character_t *string,
+     size_t string_size,
+     uint64_t *value_64bit,
+     libcerror_error_t **error )
+{
+	static char *function              = "${tools_name}_system_string_decimal_copy_to_64_bit";
+	size_t string_index                = 0;
+	system_character_t character_value = 0;
+	uint8_t maximum_string_index       = 20;
+	int8_t sign                        = 1;
+
+	if( string == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid string.",
+		 function );
+
+		return( -1 );
+	}
+	if( string_size > (size_t) SSIZE_MAX )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_VALUE_EXCEEDS_MAXIMUM,
+		 "%s: invalid string size value exceeds maximum.",
+		 function );
+
+		return( -1 );
+	}
+	if( value_64bit == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid value 64-bit.",
+		 function );
+
+		return( -1 );
+	}
+	*value_64bit = 0;
+
+	if( string[ string_index ] == (system_character_t) '-' )
+	{
+		string_index++;
+		maximum_string_index++;
+
+		sign = -1;
+	}
+	else if( string[ string_index ] == (system_character_t) '+' )
+	{
+		string_index++;
+		maximum_string_index++;
+	}
+	while( string_index < string_size )
+	{
+		if( string[ string_index ] == 0 )
+		{
+			break;
+		}
+		if( string_index > (size_t) maximum_string_index )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+			 LIBCERROR_ARGUMENT_ERROR_VALUE_TOO_LARGE,
+			 "%s: string too large.",
+			 function );
+
+			return( -1 );
+		}
+		*value_64bit *= 10;
+
+		if( ( string[ string_index ] >= (system_character_t) '0' )
+		 && ( string[ string_index ] <= (system_character_t) '9' ) )
+		{
+			character_value = (system_character_t) ( string[ string_index ] - (system_character_t) '0' );
+		}
+		else
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_UNSUPPORTED_VALUE,
+			 "%s: unsupported character value: %" PRIc_SYSTEM " at index: %d.",
+			 function,
+			 string[ string_index ],
+			 string_index );
+
+			return( -1 );
+		}
+		*value_64bit += character_value;
+
+		string_index++;
+	}
+	if( sign == -1 )
+	{
+		*value_64bit *= (uint64_t) -1;
+	}
+	return( 1 );
+}
+
---
+++ data/source/yaltools/yaltools_system_string.h
@@ -0,0 +1,45 @@
+/*
+ * System character string functions
+ *
+ * Copyright (C) ${copyright}, ${tools_authors}
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined( _${tools_name_upper_case}_SYSTEM_STRING_H )
+#define _${tools_name_upper_case}_SYSTEM_STRING_H
+
+#include <common.h>
+#include <types.h>
+
+#include "${tools_name}_libcerror.h"
+
+#if defined( __cplusplus )
+extern "C" {
+#endif
+
+int ${tools_name}_system_string_decimal_copy_to_64_bit(
+     const system_character_t *string,
+     size_t string_size,
+     uint64_t *value_64bit,
+     libcerror_error_t **error );
+
+#if defined( __cplusplus )
+}
+#endif
+
+#endif /* !defined( _${tools_name_upper_case}_SYSTEM_STRING_H ) */
+
