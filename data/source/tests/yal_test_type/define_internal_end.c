38c25577f9e8c482c360c4e9c4e8f2ca51aa74d4
Joachim Metz <joachim.metz@gmail.com>
2017-08-07 08:17:59 +0200

    Worked on source generate script

--- data/source/tests/yal_test_type/define_internal_end.c
+++ data/source/tests/yal_test_type/define_internal_end.c
@@ -1,2 +1,2 @@
-#endif /* defined( __GNUC__ ) && !defined( _${library_name_suffix_upper_case}_DLL_IMPORT ) */
+#endif /* defined( __GNUC__ ) && !defined( ${library_name_upper_case}_DLL_IMPORT ) */
 
