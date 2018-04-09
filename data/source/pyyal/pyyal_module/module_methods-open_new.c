e6ce6d8f9636d934341ac30c09e64db7f6885c93
Joachim Metz <joachim.metz@gmail.com>
2018-04-09 08:35:13 +0200

    Worked on source generate script

--- data/source/pyyal/pyyal_module/module_methods-open_new.c
+++ data/source/pyyal/pyyal_module/module_methods-open_new.c
@@ -1,12 +1,12 @@
 	{ "open",
-	  (PyCFunction) pylnk_open_new_${signature_type},
+	  (PyCFunction) ${python_module_name}_open_new_${signature_type},
 	  METH_VARARGS | METH_KEYWORDS,
 	  "open(filename, mode='r') -> Object\n"
 	  "\n"
 	  "Opens a ${signature_type}." },
 
 	{ "open_file_object",
-	  (PyCFunction) pylnk_open_new_${signature_type}_with_file_object,
+	  (PyCFunction) ${python_module_name}_open_new_${signature_type}_with_file_object,
 	  METH_VARARGS | METH_KEYWORDS,
 	  "open_file_object(file_object, mode='r') -> Object\n"
 	  "\n"
