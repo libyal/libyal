71c8555c349f75af2e5df13d73d606bd6b74f301
Joachim Metz <joachim.metz@gmail.com>
2018-09-25 07:33:10 +0200

    Worked on source generate script

--- data/source/tests/test_yalinfo.sh
+++ data/source/tests/test_yalinfo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Info tool testing script
 #
-# Version: 20180828
+# Version: 20180924
 
 EXIT_SUCCESS=0;
 EXIT_FAILURE=1;
@@ -91,7 +91,7 @@ do
 	then
 		for INPUT_FILE in `cat $${TEST_SET_DIRECTORY}/files | sed "s?^?$${TEST_SET_INPUT_DIRECTORY}/?"`;
 		do
-			run_test_on_input_file_with_options "$${TEST_SET_DIRECTORY}" "${library_name_suffix}info" "with_stdout_reference" "$${OPTION_SETS}" "$${TEST_EXECUTABLE}" "$${INPUT_FILE}";
+			run_test_on_input_file_with_options "$${TEST_SET_DIRECTORY}" "${library_name_suffix}info" "with_stdout_reference" "$${OPTION_SETS}" "$${TEST_EXECUTABLE}" "$${INPUT_FILE}" "$${OPTIONS}";
 			RESULT=$$?;
 
 			if test $${RESULT} -ne $${EXIT_SUCCESS};
