b0df146b93bd6f811cff08880b6adfba6204a580
Joachim Metz <joachim.metz@gmail.com>
2020-05-07 07:20:42 +0200

    Worked on source generate script

--- data/source/tests/pyyal_test_type/data_range_file_object.py
+++ data/source/tests/pyyal_test_type/data_range_file_object.py
@@ -12,7 +12,7 @@ class DataRangeFileObject(object):
     """
     super(DataRangeFileObject, self).__init__()
     self._current_offset = 0
-    self._file_object = open(unittest.source, "rb")
+    self._file_object = open(path, "rb")
     self._range_offset = range_offset
     self._range_size = range_size
 
