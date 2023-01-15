12cb0394d548b7482b8ccd4d305259b1fbe4fcb5
Joachim Metz <joachim.metz@gmail.com>
2023-01-15 08:03:28 +0100

    Worked on source generate script

--- data/source/yaltools/mount_file_system/includes-start.c
+++ data/source/yaltools/mount_file_system/includes-start.c
@@ -9,14 +9,11 @@
 #include <sys/stat.h>
 #endif
 
-#if defined( TIME_WITH_SYS_TIME )
+#if defined( HAVE_SYS_TIME_H )
 #include <sys/time.h>
-#include <time.h>
-#elif defined( HAVE_SYS_TIME_H )
-#include <sys/time.h>
-#else
-#include <time.h>
 #endif
+
+#include <time.h>
 
 #include "mount_file_system.h"
 #include "${tools_name}_${library_name}.h"
